3ed48e35d5eb27352868118949f05c37dbed8694
dasiob <luongbui263@gmail.com>
2022-07-22 12:53:15 +0700

    tach file controller

--- src/main/java/com/example/demo/SpringDemoApplication.java
+++ src/main/java/com/example/demo/SpringDemoApplication.java
@@ -6,16 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello(){
-		return "Hello World";
-	}
+
 
 }
